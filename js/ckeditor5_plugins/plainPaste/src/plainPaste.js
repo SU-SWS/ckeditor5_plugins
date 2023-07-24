a18431432648658e9fa8069cb212bd961ba2453d
Mike Decker <pookmish@gmail.com>
2023-07-24 15:55:17 -0700

    Allow specific tags when pasting but clean them up

--- js/ckeditor5_plugins/plainPaste/src/plainPaste.js
+++ js/ckeditor5_plugins/plainPaste/src/plainPaste.js
@@ -1,5 +1,4 @@
 import { Plugin } from 'ckeditor5/src/core';
-import plainTextToHtml from '@ckeditor/ckeditor5-clipboard/src/utils/plaintexttohtml';
 
 export default class PlainPaste extends Plugin {
 
@@ -22,10 +21,42 @@ export default class PlainPaste extends Plugin {
 
     const editingView = editor.editing.view;
 
+    const allowedTags = [
+      'p',
+      'a',
+      'h2',
+      'h3',
+      'h4',
+      'h5',
+      'ul',
+      'ol',
+      'li',
+      'table',
+      'thead',
+      'tbody',
+      'th',
+      'tr',
+      'td',
+    ];
+    const tagRegex = new RegExp('</?(?!(?:' + allowedTags.join('|') + ')\\b)[a-z](?:[^>"\']|"[^"]*"|\'[^\']*\')*>', 'g');
+
     editingView.document.on('clipboardInput', (evt, data) => {
       const dataTransfer = data.dataTransfer;
-      let content = plainTextToHtml(dataTransfer.getData('text/plain'));
-      data.content = this.editor.data.htmlProcessor.toView(content);
+      const html = dataTransfer.getData('text/html');
+      // Replace new lines to a single line string.
+      const cleanHtml = html.replace(/(?:\r\n|\r|\n)/g, ' ')
+        // Replace extra spaces.1
+        .replace(/ +/g, ' ')
+        // Remove space between tags.
+        .replace(/> </g, '><')
+        // Remove all tags we don't want to paste in.
+        .replace(tagRegex, '')
+        // Remove the most common attributes from the tags above.
+        .replace(/ style="[^\"]*"/g, '')
+        .replace(/ id="[^\"]*"/g, '')
+        .replace(/ class="[^\"]*"/g, '')
+        .replace(/ data-([a-z\-]+)="[^\"]*"/g, '');
+      data.content = this.editor.data.htmlProcessor.toView(cleanHtml);
     });
   }
 
