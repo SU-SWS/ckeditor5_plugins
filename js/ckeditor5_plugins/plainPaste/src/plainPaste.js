3e5168121fb12d9f7c784bdff49d12d98eded57c
Mike Decker <pookmish@gmail.com>
2023-07-24 16:24:33 -0700

    Use sanitize html package

--- js/ckeditor5_plugins/plainPaste/src/plainPaste.js
+++ js/ckeditor5_plugins/plainPaste/src/plainPaste.js
@@ -1,4 +1,5 @@
 import { Plugin } from 'ckeditor5/src/core';
+import sanitizeHtml from 'sanitize-html';
 
 export default class PlainPaste extends Plugin {
 
@@ -21,41 +22,30 @@ export default class PlainPaste extends Plugin {
 
     const editingView = editor.editing.view;
 
-    const allowedTags = [
-      'p',
-      'a',
-      'h2',
-      'h3',
-      'h4',
-      'h5',
-      'ul',
-      'ol',
-      'li',
-      'table',
-      'thead',
-      'tbody',
-      'th',
-      'tr',
-      'td',
-    ];
-    const tagRegex = new RegExp('</?(?!(?:' + allowedTags.join('|') + ')\\b)[a-z](?:[^>"\']|"[^"]*"|\'[^\']*\')*>', 'g');
-
     editingView.document.on('clipboardInput', (evt, data) => {
       const dataTransfer = data.dataTransfer;
       const html = dataTransfer.getData('text/html');
-      // Replace new lines to a single line string.
-      const cleanHtml = html.replace(/(?:\r\n|\r|\n)/g, ' ')
-        // Replace extra spaces.1
-        .replace(/ +/g, ' ')
-        // Remove space between tags.
-        .replace(/> </g, '><')
-        // Remove all tags we don't want to paste in.
-        .replace(tagRegex, '')
-        // Remove the most common attributes from the tags above.
-        .replace(/ style="[^\"]*"/g, '')
-        .replace(/ id="[^\"]*"/g, '')
-        .replace(/ class="[^\"]*"/g, '')
-        .replace(/ data-([a-z\-]+)="[^\"]*"/g, '');
+
+      const cleanHtml = sanitizeHtml(html, {
+        allowedTags: [
+          'p',
+          'a',
+          'h2',
+          'h3',
+          'h4',
+          'h5',
+          'ul',
+          'ol',
+          'li',
+          'table',
+          'thead',
+          'tbody',
+          'th',
+          'tr',
+          'td',
+        ],
+        allowedAttributes: {a: ['href']}
+      })
       data.content = this.editor.data.htmlProcessor.toView(cleanHtml);
     });
   }
