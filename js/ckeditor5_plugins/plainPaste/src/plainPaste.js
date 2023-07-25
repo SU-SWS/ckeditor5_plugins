1f0557a7ebe7c0cf65a011dbb17e7359607e856e
Mike Decker <pookmish@gmail.com>
2023-07-25 13:30:36 -0700

    Refactor paste event to use the clipboard pipeline for google and word docs

--- js/ckeditor5_plugins/plainPaste/src/plainPaste.js
+++ js/ckeditor5_plugins/plainPaste/src/plainPaste.js
@@ -18,43 +18,55 @@ export default class PlainPaste extends Plugin {
    * @inheritdoc
    */
   init() {
+    const sanitizeOptions = {
+      allowedTags: [
+        'p',
+        'a',
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'ul',
+        'ol',
+        'li',
+        'table',
+        'thead',
+        'tbody',
+        'th',
+        'tr',
+        'td',
+        'b',
+        'i',
+        'strong',
+        'em',
+        'dl',
+        'dt',
+        'dd',
+        'blockquote',
+        'code',
+      ],
+      allowedAttributes: { a: ['href'] },
+    };
+
     const editor = this.editor;
+    const document = editor.editing.view.document;
 
-    const editingView = editor.editing.view;
+    editor.plugins.get('ClipboardPipeline').on('inputTransformation', (event, data) => {
+      const html = data.dataTransfer.getData('text/html');
+      // Fall back to the plain text but keep the line breaks a <p> tags.
+      const plain = ('<p>' + data.dataTransfer.getData('text/plain').replace(/(?:\r\n|\r|\n)/g, '</p><p>') + '</p>')
+        .replace(/<p><\/p>/, '');
+      const contents = html.length > 0 ? html : plain;
 
-    editingView.document.on('clipboardInput', (evt, data) => {
-      const dataTransfer = data.dataTransfer;
-      const cleanHtml = sanitizeHtml(dataTransfer.getData('text/html'), {
-        allowedTags: [
-          'p',
-          'a',
-          'h2',
-          'h3',
-          'h4',
-          'h5',
-          'ul',
-          'ol',
-          'li',
-          'table',
-          'thead',
-          'tbody',
-          'th',
-          'tr',
-          'td',
-          'b',
-          'i',
-          'strong',
-          'em',
-          'dl',
-          'dt',
-          'dd',
-          'blockquote',
-          'code',
-        ],
-        allowedAttributes: { a: ['href'] },
-      });
-      data.content = this.editor.data.htmlProcessor.toView(cleanHtml);
-    });
+      const cleanHtml = sanitizeHtml(contents, sanitizeOptions);
+
+      data.content = this.editor.data.htmlProcessor.toView(cleanHtml.replace(/<p><\/p>/g, ''));
+
+      // Act like we've already transfored the input. This prevrents the
+      // paste-from-office plugin from doing more stuff that basically reverts
+      // the work above.
+      data._isTransformedWithPasteFromOffice = true;
+    }, { priority: 'highest' });
   }
 
 }
